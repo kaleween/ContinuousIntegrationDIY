cb70763ac8cfcc160ed14d7164bb70d3b13eecad
kaleween <kaleween@gmail.com>
2020-07-23 14:25:27 +0200

    add .githooks

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ContinuousIntegrationDIY
     {
         public static void Main()
         {
-                Console.WriteLine("Version : " + Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("Version : " + Assembly.GetExecutingAssembly().GetName().Version);
             Console.WriteLine("Hello World!");
         }
     }
