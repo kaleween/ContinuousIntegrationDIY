7b369bd2e761771c98b138018077d01bdc8096fe
kaleween <kaleween@gmail.com>
2020-07-23 11:53:32 +0200

    test pre push

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -23,8 +23,6 @@ namespace Tests
                 "Version : " + Assembly.GetAssembly(typeof(Program))?.GetName().Version + Environment.NewLine +
                 "Hello World!" + Environment.NewLine, 
                 consoleOutput.ToString());
-            
-            consoleOutput.ToString("test pre-push");
-        }
+            }
     }
 }
